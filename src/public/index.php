6b616a106e67a23c30200bfc09e9664b00858341
Dima <spirit.92@ukr.net>
2019-12-12 03:06:43 +0200

    Final

---
+++ src/public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+$con = mysqli_connect("mysql",'root','rootpass',"beetroot");
+mysqli_set_charset($con,"utf8");
+
+if (mysqli_connect_error()){
+    echo "failed to connect MySQL".mysqli_connect_error();
+}
