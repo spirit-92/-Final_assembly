b2a74a85b4fcbfeac09ccb22414f1190d05bd6b3
Dima <spirit.92@ukr.net>
2019-12-23 22:22:05 +0200

    Routes

--- src/app/GetWeatherFoo.php
+++ src/app/GetWeatherFoo.php
@@ -2,6 +2,7 @@
 
 
 namespace App;
+
 use Illuminate\Support\Facades\OptionsApi;
 
 class GetWeatherFoo
@@ -11,6 +12,7 @@ class GetWeatherFoo
         $url = 'http://api.openweathermap.org/data/2.5/weather';
         $option = OptionsApi\OptionsApi::getOptionsApi();
         $option['q'] = $name;
+//        $hostname = env("OPTIONS_API_WEATHER", "somedefaultvalue");
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_URL, $url.'?'.http_build_query($option) );
