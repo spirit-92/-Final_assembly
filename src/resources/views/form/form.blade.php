5f880787feb09caab0695969cb1e4a6c9140692d
Dima <spirit.92@ukr.net>
2020-01-09 15:08:47 +0200

    Form

---
+++ src/resources/views/form/form.blade.php
@@ -0,0 +1,209 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!--Стили BootsTrap-->
+    <link rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          crossorigin="anonymous">
+
+    <!--Мои стили-->
+    <link rel="stylesheet" href="../style.css">
+
+    <!--Заголовок страницы-->
+    <title>Создание фейкового пользователя</title>
+</head>
+
+<body>
+<div class="container-fluid
+            no-gutters
+            row
+            p-0">
+    <!--Навигация-->
+    <div class="border-right
+                col-2">
+        <ul class="flex-column
+                   nav">
+            <li class="border-bottom
+                       nav-item">
+                <a class="list-group-item-action
+                          list-group-item
+                          text-secondary
+                          border-0
+                          nav-link"
+                   href="usersList.html">Список пользователей</a>
+            </li>
+
+            <li class="border-bottom
+                       nav-item">
+                <a class="list-group-item-action
+                          list-group-item
+                          text-secondary
+                          border-0
+                          nav-link"
+                   href="eventsList.html">Список мероприятий</a>
+            </li>
+
+            <li class="border-bottom
+                       nav-item">
+                <a class="list-group-item-action
+                          list-group-item
+                          text-secondary
+                          border-0
+                          nav-link"
+                   href="reportsList.html">Список и просмотр жалоб</a>
+            </li>
+
+            <li class="border-bottom
+                       nav-item">
+                <a class="list-group-item-action
+                          list-group-item
+                          text-secondary
+                          border-0
+                          nav-link"
+                   href="pushNotification.html">Отправка уведомления</a>
+            </li>
+
+            <li class="border-bottom
+                       nav-item">
+                <a class="list-group-item-action
+                          list-group-item
+                          text-secondary
+                          border-0
+                          nav-link"
+                   href="sliderProperties.html">Настройка слайдера</a>
+            </li>
+
+            <li class="border-bottom
+                       nav-item">
+                <a class="list-group-item-action
+                          list-group-item
+                          text-secondary
+                          border-0
+                          nav-link"
+                   href="referalCodesList.html">Список реферальных кодов</a>
+            </li>
+
+            <li class="border-bottom
+                       nav-item">
+                <a class="list-group-item-action
+                          list-group-item
+                          text-secondary
+                          border-0
+                          nav-link"
+                   href="../index.html">Выход</a>
+            </li>
+        </ul>
+    </div>
+
+    <!--Создание фейкового пользователя-->
+    <div class="col-10
+                no-gutters">
+        <div class="border-bottom
+                    no-gutters
+                    col-12
+                    mb-2">
+            <p class="text-secondary
+                      text-center
+                      py-2
+                      pb-3
+                      m-0
+                      h3">Создание фейкового пользователя</p>
+        </div>
+
+        <form action="#"
+              class="text-secondary
+                     border-bottom
+                     p-2">
+            <div class="form-group">
+                <label for="nameInput">Имя</label>
+                <input type="text"
+                       class="form-control"
+                       id="nameInput"
+                       placeholder="Введите имя">
+            </div>
+
+            <div class="form-group">
+                <label for="userProfilePhoto">Фото</label>
+                <div class="userProfilePhoto
+                    mb-2">
+                    <img src="http://www.bienhealth.com/img/articles/5-genov-forma-chelovecheskogo-litsa.jpg"
+                         class="image-fluid"
+                         alt="userProfilePhoto">
+                </div>
+
+                <div class="custom-file">
+                    <input type="file" class="custom-file-input" id="userProfilePhoto" required>
+                    <label class="custom-file-label"
+                           for="userProfilePhoto">Выберите файл</label>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="dateBirthday">Дата рождения</label>
+                <input type="date"
+                       id="dateBirthday"
+                       class="form-control">
+            </div>
+
+            <div class="form-group">
+                <label for="userGender">Пол</label>
+                <select id="userGender"
+                        class="form-control">
+                    <option>Мужской</option>
+                    <option>Женский</option>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label for="userCountry">Страна</label>
+                <select id="userCountry"
+                        class="form-control">
+                    <option>Страна 1</option>
+                    <option>Страна 2</option>
+                    <option>Страна 3</option>
+                    <option>Страна 4</option>
+                    <option>Страна 5</option>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label for="userCity">Город</label>
+                <input type="text"
+                       id="userCity"
+                       class="form-control"
+                       placeholder="Укажите город">
+            </div>
+
+            <div class="form-group">
+                <label for="userAboutYourself">О себе</label>
+                <textarea class="form-control"
+                          id="userAboutYourself"
+                          rows="3"
+                          placeholder="Расскажите немного о себе..."></textarea>
+            </div>
+
+            <button class="btn
+                   btn-primary
+                   btn-success"
+                    type="button"
+                    aria-pressed="true">Сохранить</button>
+        </form>
+    </div>
+</div>
+
+<!--Скрипты Bootstrap-->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
+
