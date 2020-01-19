c1af7cafd4ec1194554b1af6c7a454562fa9b8f7
Dima <spirit.92@ukr.net>
2020-01-19 11:45:12 +0200

    Bd_seeds

--- src/database/seeds/UsersTFakebleSeeder.php
+++ src/database/seeds/UsersTFakebleSeeder.php
@@ -15,35 +15,35 @@ class UsersTFakebleSeeder extends Seeder
     public function run()
     {
         $faker = Faker::create();
-        for ($i1 = 1; $i1 <= 10; $i1++) {
+        for ($i = 1; $i <= 10; $i++) {
             DB::table('authors')->insert([
                 'name' => $faker->name
             ]);
         }
-        for ($i2 = 1; $i2 <= 10; $i2++) {
+        for ($i = 1; $i <= 10; $i++) {
             DB::table('owner')->insert([
-                'id'=> $i2,
+                'id'=> $i,
                 'name' => $faker->name
             ]);
         }
-        for ($i3 = 1; $i3 <= 10; $i3++) {
+        for ($i = 1; $i <= 10; $i++) {
                 DB::table('country')->insert([
-                'id'=> $i3,
+                'id'=> $i,
                 'name' => $faker->country
             ]);
         }
-        for ($i4 = 1; $i4 <= 10; $i4++) {
+        for ($i = 1; $i <= 10; $i++) {
                 DB::table('city')->insert([
-                'id'=> $i4,
+                'id'=> $i,
                 'name' => $faker->city,
-                'country_id'=>$i4
+                'country_id'=>$i
             ]);
         }
-        for ($i5 = 1; $i5 <= 10; $i5++) {
+        for ($i = 1; $i <= 10; $i++) {
             DB::table('auditions')->insert([
-                'id'=> $i5,
-                'city' => $i5,
-                'owner'=>$i5
+                'id'=> $i,
+                'city' => $i,
+                'owner'=>$i
             ]);
         }
     }
