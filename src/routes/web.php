8fc3d5b67bd7da2b702ad41302b1ca9ab7922568
Dima <spirit.92@ukr.net>
2020-02-03 17:15:19 +0200

    api

--- src/routes/web.php
+++ src/routes/web.php
@@ -12,5 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return response([
+        'headers' => 'header',
+        'body' => 'sdadasdad',
+        'status' => 200
+    ]);
 });
