6b5fc9bf5c9613fda60f316d75316bf4de1131e6
Dima <spirit.92@ukr.net>
2020-02-05 06:28:48 +0200

    MyApi(cleardebug)

--- src/app/Http/Controllers/Api/AuthoriseController.php
+++ src/app/Http/Controllers/Api/AuthoriseController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Requests\AuthoriseRequest;
 use App\Services\AuthoriseUserServices;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class AuthoriseController extends Controller
